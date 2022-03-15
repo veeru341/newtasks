753d283b6e86ef60011bd9dfa6fb2362b0afd7dd
Veeru <iamveeru1994@gmail.com>
2022-03-16 00:04:52 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,83 @@
-import React, { useState } from 'react';
-import Button from '@material-ui/core/Button';
+import React, { useState, useEffect } from "react";
 import "./App.css"
+import { FiX } from "react-icons/fi";
 
-export default function App(props) {
-  const [isShowing, setIsShowing] = useState(true);
+export default function Todo() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
 
-  const handleClick = () => {
-      setIsShowing(!isShowing)
+  useEffect(() => {
+    if (localStorage.getItem("localTasks")) {
+      const storedList = JSON.parse(localStorage.getItem("localTasks"));
+      setTasks(storedList);
+    }
+  }, [])
+
+  const addTask = (e) => {
+    if (task) {
+      const newTask = { id: new Date().getTime().toString(), title: task };
+      setTasks([...tasks, newTask]);
+      localStorage.setItem("localTasks", JSON.stringify([...tasks, newTask]));
+      setTask("");
+    }
+  };
+
+  const handleDelete = (task) => {
+    const deleted = tasks.filter((t) => t.id !== task.id);
+    setTasks(deleted);
+    localStorage.setItem("localTasks", JSON.stringify(deleted))
   }
-    
+
+  const handleClear = () => {
+    setTasks([]);
+    localStorage.removeItem("localTasks");
+  }
   return (
-    <div>
-      <center>
-        <h2>Hi this is react</h2>
-          <Button onClick={handleClick} variant="contained" color="primary">Primary</Button><br />
-          {isShowing ? 
-          <img className="image" src="https://images.pexels.com/photos/381739/pexels-photo-381739.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" /> : null}
-      </center>
-    </div>
-  )
+    <center>
+      <div className='container'>
+        <h2 className="heading">Daily Tasks-List</h2>
+        <div className="">
+          <input
+            name="task"
+            type="text"
+            value={task}
+            placeholder="Write your task..."
+            className="input-form"
+            onChange={(e) => setTask(e.target.value)}
+          />
+          <button
+            className="btn"
+            onClick={addTask}
+          >
+            ADD
+          </button>
+        </div>
+        <div className="badge">
+          You have
+          {!tasks.length
+            ? " no tasks"
+            : tasks.length === 1
+              ? " 1 task"
+              : tasks.length > 1
+                ? ` ${tasks.length} tasks`
+                : null}
+        </div>
+        {tasks.map((task) => (
+          <div className="list-items">
+            <p className="task">
+              {task.title}
+            </p>
+            <FiX onClick={() => handleDelete(task)} className="btn-1" />
+          </div>
+        ))}
+        {!tasks.length ? null : (
+          <div>
+            <button className="btn-2" onClick={() => handleClear()}>
+              Clear All
+            </button>
+          </div>
+        )}
+      </div>
+    </center>
+  );
 }
